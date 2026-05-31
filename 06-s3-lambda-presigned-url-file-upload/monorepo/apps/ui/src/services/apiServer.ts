3d983b95ffee1ef8bbe27663f7a0dc836a18be85
Your Name <you@example.com>
2026-05-31 09:35:10 +0100

    added L06 presigned urls

---
+++ 06-s3-lambda-presigned-url-file-upload/monorepo/apps/ui/src/services/apiServer.ts
@@ -0,0 +1,12 @@
+import { config } from "../config";
+
+export const checkApiServerHealth = async () => {
+  const response = await fetch(`${config.apiBaseUrl}health`);
+
+  if (!response.ok) {
+    return false;
+  }
+
+  const body = await response.text();
+  return body.trim() === "Healthy!";
+};
